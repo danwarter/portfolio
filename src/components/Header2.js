e6050d399df69e05905edeecd9dbd862fb716091
Dansbeerlist <danielpwarter@gmail.com>
2019-09-19 10:00:08 -0700

    fixed download and email links for desktop and mobile

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -9,9 +9,9 @@ const Header2 = props => (
         <Link to="/#Work">Work</Link>
         <Link to="/#Contact">Contact</Link>
         <Link to="/#AboutMe">About</Link>
-        <Link to="/download"><button>Resume</button></Link>
-        <Link to="/download" media="(max-width: 800px)"><img src={'https://d1sz9tkli0lfjq.cloudfront.net/items/1Y1k2S3r3m1D0B2l0c3O/email-mobile.svg'} /></Link>
-        <Link to="/download" media="(max-width: 800px)"><img src={'https://d1sz9tkli0lfjq.cloudfront.net/items/0w1l0u2T0j0P223g1L15/resume-mobile.svg'} /></Link>
+        <a href="https://d1sz9tkli0lfjq.cloudfront.net/items/3m2c2i2o0G1S230t292m/dwarter-resume.pdf" download><button>Resume</button></a>
+        <a href="mailto:danielpwarter@gmail.com"><img src={'https://d1sz9tkli0lfjq.cloudfront.net/items/1Y1k2S3r3m1D0B2l0c3O/email-mobile.svg'} /></a>
+        <a href="https://d1sz9tkli0lfjq.cloudfront.net/items/3m2c2i2o0G1S230t292m/dwarter-resume.pdf" download><img src={'https://d1sz9tkli0lfjq.cloudfront.net/items/0w1l0u2T0j0P223g1L15/resume-mobile.svg'} /></a>
         </div>
     </div>
     
